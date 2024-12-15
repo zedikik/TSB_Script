2c424c44aceb846a469beb0e9af1b6abd917a95b
zedikik <mogilatyyd@gmail.com>
2024-12-15 15:46:33 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -18,11 +18,15 @@ local selectedChar = ""
 if not _G.killWorkChars then
 	_G.killWorkChars = {"Hunter", "Cyborg", "Ninja", "Esper", "Blade"}
 	_G.killWhiteList = true
-	_G.killactivated = true -- false to disable
-	_G.killSafeSelf = true -- false to disable
+	_G.killactivated = true
+	_G.killSafeSelf = true
 	_G.killSafeProp = 15
 	_G.killChargeUp = false 
 	_G.killKilling = false
+	
+	_G.adcActivated = false -- anti death counter
+	_G.isDeath = false -- check if player anim == 11343250001 (death counter anim)
+	_G.adcQuotes = 1 -- wait time (1 = no wait; 2 = 4 seconds fakeout; 3 = 8 seconds long fakeout)
 end
 
 local Window = Rayfield:CreateWindow({
@@ -38,18 +42,18 @@ local Window = Rayfield:CreateWindow({
 	ConfigurationSaving = {
 		Enabled = true,
 		FolderName = "Tsb_Script", -- Create a custom folder for your hub/game
-		FileName = "Big Hub"
+		FileName = "config"
 	},
 
 	KeySystem = true, -- Set this to true to use our key system
 	KeySettings = {
 		Title = "Key",
 		Subtitle = "Key System",
-		Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+		Note = "Key is Skuff", -- Use this to tell the user how to get a key
 		FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
 		SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
 		GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-		Key = {"Skuff"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+		Key = {"Skuff", "skuff"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
 	}
 })
 
@@ -100,6 +104,18 @@ local killSafePropSlider = Tab2:CreateSlider({
 	Callback = function(Value)
 		print(Value)
 		_G.killSafeProp = Value
+	end,
+})
+
+local Section3 = Tab2:CreateSection("Functions")
+
+local antiDeathCounterToggle = Tab2:CreateToggle({
+	Name = "Toggle Kill Stealer",
+	CurrentValue = false,
+	Flag = "ADCToggle", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+	Callback = function(Value)
+		print(Value)
+		_G.adcActivated = Value
 	end,
 })
 
@@ -526,6 +542,32 @@ RunService.Heartbeat:Connect(function()
 
 		if _G.killActivated == true and killWorking == true and _G.killKilling == false and localPlayer.Character:FindFirstChild("HumanoidRootPart") then
 			localPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(0, 200, 0)
+		end
+	end
+	
+	if localPlayer.Character then
+		local humanoid = localPlayer.Character:FindFirstChildOfClass("Humanoid")
+		local animator = humanoid:WaitForChild("Animator")
+		if humanoid and animator and (humanoid.Health ~= 0 and humanoid.Health ~= 1) then
+			for _, animation in pairs(humanoid:GetPlayingAnimationTracks()) do
+				if animation then
+					print(humanoid.Name, animation, animation.AnimationId)
+				end
+			end
+			
+			for _, animation in pairs(animator:GetPlayingAnimationTracks()) do
+				if animation then
+					print(animator.Name, animation, animation.AnimationId)
+				end
+			end
+		else
+			warn(humanoid, animator, humanoid.Health, (humanoid.Health ~= 0 and humanoid.Health ~= 1))
+		end
+	end
+	
+	if localPlayer.Character then
+		if _G.isDeath == true then
+			warn("Death Countered LOL bozo kid")
 		end
 	end
 end)
